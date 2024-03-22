82fe43e6bcb127f801ae4191243e72e0e0ced415
Norman Jäckel <contact@normanjaeckel.de>
2024-03-22 22:25:54 +0100

    Add extra packages.

--- devenv.nix
+++ devenv.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }:
 
 {
+  packages = [
+    pkgs.llvmPackages_9.libcxxClang
+  ];
+
   enterShell = ''
     ROCDIR="$(pwd)/$(find . -type d -iname 'roc_nightly*' | head -n 1)"
     PATH=$ROCDIR:$PATH
