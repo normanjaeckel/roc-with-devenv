78642da711767cd4d79150f866efc0f02a2d4460
Norman Jäckel <contact@normanjaeckel.de>
2024-03-22 20:17:56 +0100

    Added some details.

--- devenv.nix
+++ devenv.nix
@@ -3,8 +3,6 @@
 {
   enterShell = ''
     ROCDIR="$(pwd)/$(find . -type d -iname 'roc_nightly*' | head -n 1)"
-    # ROCDIR="$(pwd)/roc/target/release"
-    # ROCDIR="$(pwd)/roc/target/debug"
     PATH=$ROCDIR:$PATH
   '';
 }
